19c200944dc29e9cddcc126982e79754fa45a167
lip <nyimoeyan1234@gmail.com>
2026-09-08 11:24:12 +0630

    add db connection

--- index.js
+++ index.js
@@ -3,12 +3,18 @@ const app = expresss();
 const dotenv = require("dotenv").config();
 const mongoose = require('mongoose')
 const PORT = process.env.PORT || 3000;
+const connectDB = require('./config/db')
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 })
+
+connectDB()
+
 app.get('/',(req,res)=>{
   res.send('hay this is from /')
 })
+
+
 
 
