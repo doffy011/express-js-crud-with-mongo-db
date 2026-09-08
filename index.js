117148ef8b2b95852a57fd7c44ae38ad16262a92
lip <nyimoeyan1234@gmail.com>
2026-09-08 10:56:35 +0630

    nothings change

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const expresss = require("express");
 const app = expresss();
-require("dotenv").config();
+const dotenv = require("dotenv").config();
+const mongoose = require('mongoose')
 const PORT = process.env.PORT || 3000;
-
-
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 })
+app.get('/',(req,res)=>{
+  res.send('hay this is from /')
+})
+
+
