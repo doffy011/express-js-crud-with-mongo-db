1aac34ae4500c198f586dc588ad8b57fa5530c8e
lip <nyimoeyan1234@gmail.com>
2026-09-09 10:20:47 +0630

    create react frontend

--- Backend/index.js
+++ Backend/index.js
@@ -2,11 +2,14 @@ const expresss = require("express");
 const app = expresss();
 const dotenv = require("dotenv").config();
 const mongoose = require('mongoose')
+const cors = require('cors')
 
 const connectDB = require('./config/db')
 const productRoutes = require('./routes/product.routes')
 
 app.use(expresss.json());
+app.use(cors());
+
 connectDB();
 
 app.use('/api/example_product', productRoutes);
